181dc652a6fa570a778ed1f0a5f1c2cc3cd5ace3
pritambhalnor <pritambhalnor14@gmail.com>
2022-03-21 21:10:21 +0530

    first commit

---
+++ src/component/Content.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import styled from "styled-components";
+
+export default class Content extends React.Component {
+
+    // state = { num1: '', num2: '', total: '' }     // You can use a single line, or use the long way... like show below   ..
+    constructor(props) {
+        super(props)
+        this.state = { num1: '', num2: '', total: '' }
+    }
+
+
+    exe1() {
+        // console.log("Num1: ", this.state.num1, " + Num2: ", this.state.num2)
+        this.setState({ total: parseInt(this.state.num1) + parseInt(this.state.num2) })
+        // console.log(this.state)
+    }
+
+    exe2() {
+        // console.log("Num1: ", this.state.num1, " - Num2: ", this.state.num2)
+        this.setState({ total: parseInt(this.state.num1) - parseInt(this.state.num2) })
+        // console.log(this.state)
+    }
+
+    exe3() {
+        // console.log("Num1: ", this.state.num1, " * Num2: ", this.state.num2)
+        this.setState({ total: parseInt(this.state.num1) * parseInt(this.state.num2) })
+        // console.log(this.state)
+    }
+
+    exe4() {
+        // console.log("Num1: ", this.state.num1, " / Num2: ", this.state.num2)
+        this.setState({ total: parseInt(this.state.num1) / parseInt(this.state.num2) })
+        // console.log(this.state)
+    }
+
+    render() {
+        return (
+            <Wrapper className="Container">
+                <div className="row">
+                <h1 className='App-header' >Assignment 1</h1> 
+                    <div className="col-10 mx-auto col-lg-4 col-sm-8  my-5">
+                         <div >
+                            <label className="col-sm-3 col-form-label">Number 1: </label>
+                            <input type="number" className="inputStyle b" value={this.state.num1} onChange={(eve) => { this.setState({ num1: eve.target.value }) }} />
+                            <br /><br />
+                            <label className="col-sm-3 col-form-label">Number 2: </label>
+                            <input type="number" className="inputStyle b" value={this.state.num2} onChange={(eve) => { this.setState({ num2: eve.target.value }) }} />
+                            <br /><br />
+
+                            <button className='btn btn-outline-success me-2 ' onClick={() => { this.exe1() }}>Add </button>
+
+                            <button className='btn btn-outline-success me-2' onClick={() => { this.exe2() }}>Subtract </button>
+
+                            <button className='btn btn-outline-success me-2' onClick={() => { this.exe3() }}>Multiplication </button>
+
+                            <button className='btn btn-outline-success me-2' onClick={() => { this.exe4() }}>Division </button>
+
+
+                            <br /><br />
+
+                            <input type="number" readOnly={true} className="outStyle a" defaultValue={this.state.total} />
+
+                            <br /><br />
+                        </div>
+                    </div>
+                </div>
+            </Wrapper>
+        )
+    }
+}
+
+const Wrapper = styled.div`
+.b{
+    border:2px solid aqua;
+    border-radius:8px;
+}
+.a{
+    border:2px light red;
+    border-radius:8px;
+}
+`
+
+// Another way to fire the function "onClick"
+
+{/* <button onClick = { this.exe1.bind(this) } >Add Numbers</button> */ }
